3812cff21310ccb36ef11a567c57fcfd9784932f
Karan Chourasiya <karanchourasiya05@gmail.com>
2026-09-25 16:45:45 +0530

    Add files via upload
    Upload portfolio website files

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert("Thank you! Your message has been submitted successfully.");
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
